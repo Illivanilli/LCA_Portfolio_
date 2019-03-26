02ad9baca4e543ad93f1f534faab131167ee1be5
Christopher Parson <cparson23@gmail.com>
2019-03-26 02:05:42 -0700

    tictactoe commit -1

---
+++ Classroom/js/tictactoe.js
@@ -0,0 +1,44 @@
+// Variable initilization //
+const gamePiece = [ 'X' , 'O']; 
+const newGameButton = document.querySelector('#newGameButton');
+const restartGameButton = document.querySelector('#restartGameButton');
+
+let freeSpace = [''];
+// Global Variables //
+let patternX =['X'];
+let patternO =['O'];
+let square1 = document.querySelector('firstMiddleSquare');
+
+
+let gameBody= document.querySelector('gameBody');
+let currentPlayer = 1;
+const winningPatterns = ['123','147','159','258','357','369','456','789'];
+const gameBody = Array.from(document.querySelector('.gameBody'));
+let gameSpaces =['', '', '' ,'' ,'','','','','',];
+
+
+// Setups the game board //
+const gameboard = ['1', '2', '3', '4', '5' ,'6','7', '8', '9'];
+
+
+function placeMark (
+    if {gameBoard = false}
+
+    then {gameBoard = placeMarkNow })
+
+Gameboard[9]
+
+
+
+function placeMark (box){
+    var box =document.getElementById();
+}
+
+
+
+
+
+
+
+
+
