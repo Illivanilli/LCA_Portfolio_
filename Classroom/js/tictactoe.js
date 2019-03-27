0c69885a598aeee0342557615a096812acceb73b
Christopher Parson <cparson23@gmail.com>
2019-03-27 16:00:08 -0500

    ttt update

--- Classroom/js/tictactoe.js
+++ Classroom/js/tictactoe.js
@@ -1,42 +1,16 @@
-// Variable initilization //
-const gamePiece = [ 'X' , 'O']; 
-const newGameButton = document.querySelector('#newGameButton');
-const restartGameButton = document.querySelector('#restartGameButton');
 
-let freeSpace = [''];
-// Global Variables //
-let patternX =['X'];
-let patternO =['O'];
-let square1 = document.querySelector('firstMiddleSquare');
+$( document ).ready(function() {
+//jquery start after loading.    
+//Player state. Define Variables for both players. Setup board.
+    let playerOne = 'X';
+    let playerTwo = 'O'; 
+    let currentPLayer = '1'; 
+    let movesMade ='0';
+    
+    let box = $(".Square");
+    console.log(box);
 
-
-let gameBody= document.querySelector('gameBody');
-let currentPlayer = 1;
-const winningPatterns = ['123','147','159','258','357','369','456','789'];
-const gameBody = Array.from(document.querySelector('.gameBody'));
-let gameSpaces =['', '', '' ,'' ,'','','','','',];
-
-
-// Setups the game board //
-const gameboard = ['1', '2', '3', '4', '5' ,'6','7', '8', '9'];
-
-
-function placeMark (
-    if {gameBoard = false}
-
-    then {gameBoard = placeMarkNow })
-
-Gameboard[9]
-
-
-
-function placeMark (box){
-    var box =document.getElementById();
-}
-
-
-
-
+});
 
 
 
