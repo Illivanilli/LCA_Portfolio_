61d35f99c6f7e780fc99117072432eed4a7269b9
Christopher Parson <cparson23@gmail.com>
2019-03-30 21:41:22 -0700

    tictactoe

--- Classroom/js/tictactoe.js
+++ Classroom/js/tictactoe.js
@@ -6,12 +6,14 @@ $( document ).ready(function() {
     let playerTwo = 'O'; 
     let currentPlayer = '1'; 
     let movesMade ='0';
-    
+    let winner = [123, 234, 567, 147, 258, 369, 159, 357];
     let box = $(".square");
-   
-    box.on('click',function(e){
+    
+   //On click increases moveMade by one, then check whose turn it is. //
+
+    box.on('click', function(e){
         movesMade ++;
-//Whose turn
+
         if(currentPlayer ==1){
             event.target.innerHTML =playerOne;
             event.target.style.color="red";
@@ -26,9 +28,16 @@ $( document ).ready(function() {
     
         function winnerCheck (){
             if (movesMade > 4){
-                
+                $box.inArray(winner)({
+                    
+                })
+
             }
         }
+        if(moveMade ==1){
+            $('#restartGameButton').hide();
+        }
+
 });
 
 
