36e2a411af90ac11791ad6dc9a72872a484c2aa2
Christopher Parson <cparson23@gmail.com>
2019-04-03 11:44:33 -0700

    geolocation1

---
+++ Classroom/js/stressApp.js
@@ -0,0 +1,49 @@
+ $( document ).ready(function() {
+   
+
+    var box1;
+    var box2;
+    var box3;
+    var box4;
+    var count;
+
+    box1 = document.getElementsByClassName(box1);
+    box2 = document.getElementsByClassName(box2);
+    box3 = document.getElementsByClassName(box3);
+    box4 = document.getElementsByClassName(box4);
+
+$(function() {  
+    $('.box1').click(function(){
+        $(this).css('background','yellow');
+        
+      });
+    });
+ $(function() {  
+   $('.box2').click(function(){
+        $(this).css('background','tan');
+            
+    });
+    });
+ $(function() {  
+    $('.box3').click(function(){
+         $(this).css('background','red');
+                
+        });
+        });
+$(function() {  
+    $('.box4').click(function(){
+         $(this).css('background','green');
+                    
+         });
+    });
+
+    var onclick = 
+    $(function clickstotal(onclick){
+    
+
+
+
+
+    })
+
+})
